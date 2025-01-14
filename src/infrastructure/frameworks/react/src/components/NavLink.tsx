bcbd111a8adf0bff0a1d17530bb086d76ab7d725
jabsraughs <204bouaki.arthur@gmail.com>
2025-01-14 20:02:36 +0100

    mep front react

---
+++ src/infrastructure/frameworks/react/src/components/NavLink.tsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+interface NavLinkProps {
+  to: string;
+  label: string;
+  className?: string;
+}
+
+function NavLink({ to, label, className }: NavLinkProps) {
+  return (
+    <Link  to={to} className={className}> {label} </Link>
+  );
+}
+export default NavLink;
