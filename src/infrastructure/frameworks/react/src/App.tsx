76b18fe5eed656985f5022120e18dc94edaf5704
jabsraughs <204bouaki.arthur@gmail.com>
2025-01-15 21:52:54 +0100

    mep homePage

--- src/infrastructure/frameworks/react/src/App.tsx
+++ src/infrastructure/frameworks/react/src/App.tsx
@@ -1,6 +1,8 @@
-import MyHeader from './components/MyHeader'
-import './App.css'
-import { BrowserRouter } from 'react-router-dom';
+import MyHeader from "./components/MyHeader";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import MyFooter from "./components/MyFooter";
+import MyHomePage from "./components/MyHomePage";
 
 function App() {
   return (
@@ -8,11 +10,13 @@ function App() {
       <BrowserRouter>
         <div className="min-h-screen bg-white">
           <MyHeader className="w-full px-4 py-3 shadow-md" />
+          <MyHomePage />
+          <MyFooter />
         </div>
-        <h1 className='text-red-500'> test </h1>
+        <h1 className="text-red-500"> test </h1>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
